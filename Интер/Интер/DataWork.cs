bc74b11ce60f80f73b6e28f75211abfbc16d44a9
danilsocol <danilsocol-2002@mail.ru>
2021-02-20 17:20:50 +0500

    2

--- Интер/Интер/DataWork.cs
+++ Интер/Интер/DataWork.cs
@@ -6,33 +6,33 @@ namespace Интер
 {
     class DataWork
     {
-        public static void SelectOperation(string[] partsStr) // смотрим что над чем проимходят операции, переменная и переменная или переменная и число
+        public static void SelectOperation(string[] partsStr) 
         {
-            for (int j = 0; j < Data.peremenna.Count; j++) //проверяем есть ли такая переменная в листе  
+            for (int j = 0; j < Data.peremenna.Count; j++)            
             {
-                if (Data.peremenna[j] + "," == partsStr[1]) // если есть такая переменная если да то
+                if (Data.peremenna[j] + "," == partsStr[1])  
                 {
-                    bool swit = true; // проверка будет ли операция вида: переменная и переменная
+                    bool swit = true; 
 
-                    for (int k = 0; k < Data.peremenna.Count; k++) // проверяем вид операции : переменная и переменная
+                    for (int k = 0; k < Data.peremenna.Count; k++) 
                     {
-                        if (Data.peremenna[k] == partsStr[2]) // если ли вторая переменная в списке
+                        if (Data.peremenna[k] == partsStr[2]) 
                         {
-                            Data.inch[j] = Calculations(Data.inch[j], Data.inch[k]);// отправляемся в метод где будем производить операции
-                            swit = false; // если это переция вида : переменная и переменная , то переключаем swit, чтоб код не взял название переменной как значение 
+                            Data.inch[j] = Calculations(Data.inch[j], Data.inch[k]);
+                            swit = false; 
                             break;
                         }
                     }
-                    if (swit) // если это тип переменная и число то с 17 до 25 строчку то код не проводит никаких операций
+                    if (swit) 
                     {
-                        Data.inch[j] = Calculations(Data.inch[j], Convert.ToInt32(partsStr[2])); // отправляемся в метод где будем производить операции 
+                        Data.inch[j] = Calculations(Data.inch[j], Convert.ToInt32(partsStr[2])); 
                         break;
                     }
                 }
             }
         }
 
-        public static int Calculations(int numOne, int numTwo)// переменная где будут происходить операции
+        public static int Calculations(int numOne, int numTwo)
         {
             switch (Data.numOperation)
             {
@@ -48,7 +48,7 @@ namespace Интер
                 case (4):
                     return numOne * numTwo;
 
-                default: // если нет операций выдаем ошибочное число чтоб программа сдохла
+                default: 
                     return -1;
             }
         }
